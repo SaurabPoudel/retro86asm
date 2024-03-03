d0179cbb01ef3c3d898ecdd1b843811130a7a8a2
SaurabPoudel <poudelsaurab20@gmail.com>
2024-03-03 21:44:20 +0545

    test program

---
+++ sumof16bit.asm
@@ -0,0 +1,26 @@
+TITLE hello this is my first program on asm
+.MODEL SMALL
+.STACK 100H
+.DATA
+a dw 8888H
+b dw 9999H
+sum dw ?
+carry db 00H
+
+.code
+main proc
+mov ax,@data
+mov ds,ax
+mov ax,a
+add ax,b
+jnc no_carry
+mov carry,01H
+no_carry:
+mov sum,ax
+mov ah,4ch
+int 21h
+main endp
+end main
+
+
+
